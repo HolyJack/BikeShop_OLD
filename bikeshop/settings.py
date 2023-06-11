eaeb087c504529f12666207cd53c80f01249fa88
holyjack <holyjackdev@gmail.com>
2023-06-11 04:50:58 +0400

    junk files has been removed

--- bikeshop/settings.py
+++ bikeshop/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
+from decouple import config
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-z)%15u9lmbz^@u=7(04b94i8h#jtb4#g9ln_4z$ivuv*32z@4z'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
